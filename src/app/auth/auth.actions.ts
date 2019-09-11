0e8d481bbd5c754baff7f76f9102ed28a750fb41
Gihan Ubayawardana <gihan.ubayawardana@infor.com>
2019-09-11 20:29:53 +0530

    initial commit

---
+++ src/app/auth/auth.actions.ts
@@ -0,0 +1,19 @@
+import { Action } from '@ngrx/store';
+import { User } from '../model/user.model';
+
+export enum AuthActionTypes {
+  LoginAction = '[Auth] Load Auths',
+  LogoutAction = '[Auth] Load Auths',
+  
+  
+}
+
+export class Login implements Action {
+  readonly type = AuthActionTypes.LoginAction;
+  constructor(public payload : {user : User}){
+
+  }
+}
+
+
+export type AuthActions = Login;
